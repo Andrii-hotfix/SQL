dd7481411b1c002785fd0bf6ede48551e2e44f55
Andrii <dehtyaryov.ipt@gmail.com>
2017-11-27 01:18:29 +0200

    lab7 updates

--- lab7.sql
+++ lab7.sql
@@ -7,3 +7,27 @@ CREATE PROCEDURE GetEMployeesByDepartment
   EXEC GetEMployeesByDepartment 2;
 
   DROP PROCEDURE GetEMployeesByDepartment;
+
+--   // удалить 4 сотрудников
+
+  CREATE PROCEDURE Erase4Empl
+    @DepartmentID INT = 1
+    AS
+    DECLARE @i_count INT = 0
+    IF EXISTS(SELECT Department.Id FROM Department WHERE Department.Id = @DepartmentID)
+      WHILE @i_count < 4
+        BEGIN
+          DELETE TOP(1) FROM Employee WHERE Employee.Department = @DepartmentID;
+          SET @i_count = @i_count + 1;
+        END
+
+GO
+
+  SELECT * FROM Employee WHERE Employee.Department = 1;
+--
+  EXEC Erase4Empl 1;
+--
+  SELECT * FROM Employee WHERE Employee.Department = 1;
+
+  DROP PROCEDURE Erase4Empl;
+
