2f55fa0b07ff1df0e64913959343655cd46001e3
Andrii <dehtyaryov.ipt@gmail.com>
2017-12-11 20:20:55 +0200

    lab8-v1

---
+++ lab8.sql
@@ -0,0 +1,32 @@
+CREATE TRIGGER EmployeeTrigger
+  ON Employee INSTEAD OF INSERT AS
+  BEGIN
+    IF EXISTS(SELECT Employee.Name FROM Employee WHERE Employee.Name = (SELECT inserted.Name FROM inserted))
+      BEGIN
+        RAISERROR ('Error: This employee allready exists!', 1, 1)
+        ROLLBACK
+      END
+    ELSE
+      BEGIN
+        INSERT INTO Employee
+          SELECT Name, LastName, MiddleName, Gender, Birthday, Adress, Post, StartDate, EndDate, Department, Corp
+          FROM inserted
+      END
+  END
+
+SELECT * FROM Employee
+
+DROP TRIGGER EmployeeTrigger
+
+INSERT INTO Employee (Name, LastName, MiddleName, Gender, Birthday, Adress, Post, StartDate, EndDate, Department, Corp)
+    VALUES (
+        'Elena', 'Ivanova', 'NIKIFOROVA', 'f', '01.01.1991', 'Borshahivska st. 129', 2, '01.01.2003', NULL, 2, 2
+    );
+
+INSERT INTO Employee (Name, LastName, MiddleName, Gender, Birthday, Adress, Post, StartDate, EndDate, Department, Corp)
+    VALUES (
+        'MIKE', 'Ivanova', 'NIKIFOROVA', 'f', '01.01.1991', 'Borshahivska st. 129', 2, '01.01.2003', NULL, 2, 2
+    );
+
+
+DELETE FROM Employee WHERE Employee.MiddleName = 'NIKIFOROVA';
